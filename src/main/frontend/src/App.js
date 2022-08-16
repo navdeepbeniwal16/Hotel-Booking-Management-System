12b0fe5e49af96ca88e5ee2dcebb38922ffa5da4
Levi MK <levimckenzie93@gmail.com>
2022-08-16 23:39:22 +1000

    bugfix: proxy calls from react app to dev backend server

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 function App() {
+  const [number, setNumber] = useState(0);
+  useEffect(() => {
+      const fetchData = async () => {
+          const res = await fetch('api/test', {
+              headers : {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+              }
+          })
+          const json = await res.json()
+          setTimeout(() => {
+              setNumber(json.number)
+          }, 1500)
+      }
+      fetchData()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Highest number: {number}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
