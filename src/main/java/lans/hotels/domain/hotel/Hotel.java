fdcd1d9644605f1da6057631cd56bf42ba8f7783
Levi MK <levimckenzie93@gmail.com>
2022-08-23 16:34:11 +1000

    add UUID to hotel

--- src/main/java/lans/hotels/domain/hotel/Hotel.java
+++ src/main/java/lans/hotels/domain/hotel/Hotel.java
@@ -2,11 +2,16 @@ package lans.hotels.domain.hotel;
 
 public class Hotel {
     private String name;
-    public Hotel(String name) {
+    private Integer UUID;
+    public Hotel(Integer UUID, String name) {
         this.name = name;
+        this.UUID = UUID;
     }
-
     public String getName() {
         return name;
+    }
+
+    public boolean isHotel(Hotel other) {
+        return this.UUID == other.UUID;
     }
 }
--- src/test/java/lans/hotels/domain/hotel/HotelTest.java
+++ src/test/java/lans/hotels/domain/hotel/HotelTest.java
@@ -12,9 +12,26 @@ class HotelTest {
         String name = "LANS";
 
         // Act
-        Hotel hotel = new Hotel(name);
+        Hotel hotel = new Hotel(1234, name);
 
         // Assert
         Assertions.assertEquals(name, hotel.getName());
+    }
+
+    @Test
+    void Hotels_are_compared_on_UUID() {
+        // Arrange
+        String sameName = "LANS";
+        Hotel hotel1 = new Hotel(12345, sameName);
+        Hotel hotel2 = new Hotel(67890, sameName);
+        // Act
+        boolean isHotel1Hotel1 = hotel1.isHotel(hotel1);
+        boolean isHotel1Hotel2 = hotel1.isHotel(hotel2);
+        boolean isHotel2Hotel1= hotel2.isHotel(hotel1);
+
+        // Assert
+        Assertions.assertTrue(isHotel1Hotel1);
+        Assertions.assertFalse(isHotel1Hotel2);
+        Assertions.assertFalse(isHotel2Hotel1);
     }
 }
