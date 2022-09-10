8319f67cfb361bef24dbcdf18c4f13fcebf1f6f8
Levi MK <levimckenzie93@gmail.com>
2022-09-10 22:38:46 +1000

    bugfix: refactor test to use int as id for hotel

--- src/test/java/lans/hotels/domain/hotel/HotelTest.java
+++ src/test/java/lans/hotels/domain/hotel/HotelTest.java
@@ -1,19 +1,11 @@
 package lans.hotels.domain.hotel;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
 class HotelTest {
-    static UUID uuid1;
-    static UUID uuid2;
-    @BeforeAll
-    static void beforeAllTests() {
-        uuid1 = UUID.fromString("2a532027-2654-451e-866f-d8ab19e5a525");
-        uuid2 = UUID.fromString("8fe5b331-82a7-459c-a9c1-8a026577b337");
-    }
+    static final int id1 = 1;
+    static final int id2 = 2;
 
     @Test
     void Hotel_has_name() {
@@ -21,7 +13,7 @@ class HotelTest {
         String name = "LANS";
 
         // Act
-        Hotel hotel = new Hotel(uuid1, name);
+        Hotel hotel = new Hotel(id1, name);
 
         // Assert
         Assertions.assertEquals(name, hotel.getName());
@@ -31,8 +23,8 @@ class HotelTest {
     void Compare_hotels_with_different_UUIDS() {
         // Arrange
         String sameName = "LANS";
-        Hotel hotel1 = new Hotel(uuid1, sameName);
-        Hotel hotel2 = new Hotel(uuid2, sameName);
+        Hotel hotel1 = new Hotel(id1, sameName);
+        Hotel hotel2 = new Hotel(id2, sameName);
 
         // Act
         boolean isHotel1Hotel2 = hotel1.is(hotel2);
@@ -46,8 +38,8 @@ class HotelTest {
     void Compare_hotels_with_the_same_UUIDS() {
         // Arrange
         String sameName = "LANS";
-        Hotel hotel1 = new Hotel(uuid1, sameName);
-        Hotel hotel2 = new Hotel(uuid1, sameName);
+        Hotel hotel1 = new Hotel(id1, sameName);
+        Hotel hotel2 = new Hotel(id1, sameName);
 
         // Act
         boolean isHotel1Hotel2 = hotel1.is(hotel2);
