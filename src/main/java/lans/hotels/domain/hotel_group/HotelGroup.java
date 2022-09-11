d4d4a7b372fcd468f8960263aeb68f4e594691b7
Levi MK <levimckenzie93@gmail.com>
2022-09-11 17:38:26 +1000

    add: basic hotel group builder

--- src/main/java/lans/hotels/domain/hotel_group/HotelGroup.java
+++ src/main/java/lans/hotels/domain/hotel_group/HotelGroup.java
@@ -3,14 +3,23 @@ package lans.hotels.domain.hotel_group;
 import lans.hotels.domain.AbstractReferenceObject;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class HotelGroup extends AbstractReferenceObject  {
-    HashMap<Integer, BusinessDetail> businessDetails;
+    String name;
+    HashMap<String, BusinessDetail> businessDetails;
     protected HotelGroup() {
         super();
     }
-    protected HotelGroup(int id) {
+
+    protected HotelGroup(int id, String name, List<BusinessDetail> businessDetails) {
         super(id);
+        this.name = name;
+        businessDetails.stream().map(businessDetail -> this.businessDetails.put(businessDetail.type, businessDetail));
+    }
+
+    private HashMap<String, BusinessDetail> getBusinessDetails() {
+        return this.businessDetails;
     }
 }
 
--- src/main/java/lans/hotels/domain/hotel_group/HotelGroupBuilder.java
+++ src/main/java/lans/hotels/domain/hotel_group/HotelGroupBuilder.java
@@ -1,25 +1,43 @@
 package lans.hotels.domain.hotel_group;
 
 import lans.hotels.domain.IBuilder;
+import lans.hotels.domain.exceptions.ReferenceObjectException;
+
+import java.util.ArrayList;
 
 public class HotelGroupBuilder implements IBuilder<HotelGroup>  {
     HotelGroup hotelGroup;
+    String name;
+    Integer uid;
+    ArrayList<BusinessDetail> businessDetails;
 
     public HotelGroupBuilder() {
         reset();
     }
 
-    public HotelGroupBuilder(int id) {
-        hotelGroup = new HotelGroup(id);
+    public void uid(int id) {
+        this.uid = id;
     }
 
+    public void businessDetail(BusinessDetail businessDetail) {
+        this.businessDetails.add(businessDetail);
+    }
 
+    public void name(String name) {
+        this.name = name;
+    }
 
     public void reset() {
-        hotelGroup = new HotelGroup();
+        hotelGroup = null;
+        name = "Default Hotel Group Name";
+        uid = -1;
+        businessDetails = new ArrayList<>();
     }
 
     public HotelGroup getResult() {
+        if (this.hotelGroup == null) {
+            this.hotelGroup = new HotelGroup(this.uid, this.name, this.businessDetails);
+        }
         return this.hotelGroup;
     }
 }
