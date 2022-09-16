129ad8cb4500874714e7b15bc8e6eb4aea462448
denin <navdeep.beniwal1997@gmail.com>
2022-09-16 21:57:32 +1000

    wip: add basic non functioning nav bar

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,28 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import "./App.css";
+// import React, { useState, useEffect } from "react";
+
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MainNavbar } from "./components/layouts/MainNavbar";
+
 function App() {
-  const [number, setNumber] = useState(0);
-  useEffect(() => {
-      const fetchData = async () => {
-          const res = await fetch('/api/test', {
-              headers : {
-                  'Content-Type': 'application/json',
-                  'Accept': 'application/json'
-              }
-          })
-          const json = await res.json()
-          setNumber(json.number)
-      }
-      fetchData()
-  }, [])
+  // const [number, setNumber] = useState(0);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("/api/test", {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Accept: "application/json",
+  //       },
+  //     });
+  //     const json = await res.json();
+  //     setNumber(json.number);
+  //   };
+  //   fetchData();
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Highest number: {number}
-        </p>
+    <div>
+      <header>
+        <MainNavbar></MainNavbar>
       </header>
     </div>
   );
---
+++ src/main/frontend/src/components/layouts/MainNavbar.js
@@ -0,0 +1,30 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+export const MainNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        {/* <Navbar.Brand href="#">Navbar scroll</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Bookings</Nav.Link>
+          </Nav>
+          <Nav className="d-flex">
+            <Nav.Link href="#action2">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
