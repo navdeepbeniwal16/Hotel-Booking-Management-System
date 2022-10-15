1abb779813a76a6357424a76bb3bb2f7ee7525d2
Levi (Github) <levimckenzie93@gmail.com>
2022-10-15 23:32:28 +1100

    :sparkles: style cancelled booking toast

--- src/main/frontend/src/pages/Hotelier/BookingCard.tsx
+++ src/main/frontend/src/pages/Hotelier/BookingCard.tsx
@@ -10,15 +10,20 @@ interface BookingCardProps {
   onToastClose: () => void;
 }
 
-const BookingCard = ({ booking, dark, onCancel, showToast, onToastClose }: BookingCardProps) => {
+const BookingCard = ({
+  booking,
+  dark,
+  onCancel,
+  showToast,
+  onToastClose,
+}: BookingCardProps) => {
   return (
     <ListGroupItem
       as='li'
       className='d-flex justify-content-between align-items-start'
-
-      variant={dark ? 'dark':''}
+      variant={dark ? 'dark' : ''}
     >
-      <div className="ms-2 me-auto">
+      <div className='ms-2 me-auto'>
         <h4>{booking.customer_name}</h4>
         <div>
           <span className='fw-bold'>Booking ID: </span>
@@ -41,16 +46,24 @@ const BookingCard = ({ booking, dark, onCancel, showToast, onToastClose }: Booki
           )}
         </div>
       </div>
-      {booking.is_active ? <Button
-        onClick={() => onCancel(booking)}
-        variant='danger'
-      >
-        Cancel booking
-      </Button> : <Toast show={showToast==booking.id} onClose={onToastClose} delay={3000} autohide>
-        <Toast.Header>
-          <strong className="me-auto">Booking cancelled</strong>
-        </Toast.Header>
-      </Toast>}
+      {booking.is_active ? (
+        <Button onClick={() => onCancel(booking)} variant='danger'>
+          Cancel booking
+        </Button>
+      ) : (
+        <Toast
+          show={showToast == booking.id}
+          onClose={onToastClose}
+          delay={3000}
+          autohide
+          className='bg-success text-white'
+        >
+          <Toast.Header>
+            <strong className='me-auto'>Success!</strong>
+            </Toast.Header>
+            <Toast.Body>{booking.customer_name}{"'"}s booking has been cancelled</Toast.Body>
+        </Toast>
+      )}
     </ListGroupItem>
   );
 };
