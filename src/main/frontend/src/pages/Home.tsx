3a74e517032038b4a4a37cf29067cc01a4aa27b9
Levi (Github) <levimckenzie93@gmail.com>
2022-10-19 17:58:25 +1100

    :adhesive_bandage: show only active hotels on home page

--- src/main/frontend/src/pages/Home.tsx
+++ src/main/frontend/src/pages/Home.tsx
@@ -4,14 +4,21 @@ import HotelsList from '../components/molecules/HotelsList';
 import { useEffect, useState } from 'react';
 import Hotel from '../types/HotelType';
 import AppContext from '../context/AppContext';
+import { filter, forEach, map } from 'lodash';
 
 const Home = () => {
-  const [hotels, setHotels] = useState<Hotel[]>([]);
+  const emptyHotels: Hotel[] = [];
+  const [hotels, setHotels] = useState(emptyHotels);
   const { backend } = useContext(AppContext.GlobalContext);
   useEffect(() => {
     const fetchHotels = async () => {
-      const fetchedHotels = await backend.getAllHotels();
-      setHotels(fetchedHotels);
+      const _hotels = await backend.getAllHotels();
+      const activeHotels: Hotel[] = [];
+      forEach(_hotels, (hotel: Hotel) => {
+        if (hotel.is_active) activeHotels.push(hotel);
+      })
+      setHotels(activeHotels);
+      // setHotels([..._hotels]);
     };
     fetchHotels().catch(console.error);
   }, []);
